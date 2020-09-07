e71edd45850fb6aa1cc2ea352511a369908c4026
Engstrom <isaen688@student.liu.se>
2020-09-07 21:00:50 +0200

    Set up Express and MongoDB to be able to make simple requests to the API

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
 require('dotenv').config();
 
 const app = express();
@@ -8,15 +11,17 @@ const app = express();
 //Bodyparser Middleware
 app.use(bodyParser.json());
 
-
 // DB config 
 const db = require('./config/keys').mongoURI
 
 // Connect to Mongo
 mongoose
-    .connect(db)
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB Connected..'))
     .catch(err => console.log(err));
+
+// Use Routes 
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
