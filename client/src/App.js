1e986ce6cd43b6ea69ce608e3098cc3d6ae70ecf
Engstrom <isaen688@student.liu.se>
2020-12-05 23:42:00 +0100

    Add react helmet

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { Provider } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
 import AppNavbar from './components/AppNavbar';
 import AppFooter from './components/AppFooter';
 import GoalList from './components/GoalList';
 import ItemModal from './components/ItemModal';
+import Calendar from './components/calendar/Calendar';
 import store from './store';
 import { loadUser } from './actions/authActions';
 
@@ -15,17 +17,28 @@ import './App.css';
 class App extends Component {
 	componentDidMount() {
 		store.dispatch(loadUser());
+		
 	}
 	
 	render(){
 		return (
 			<Provider store={store}>
+				<Helmet>
+					<title>Coalendar</title>
+				</Helmet>
 				<div className="App">
 					<AppNavbar />
-						<Container style={{ marginTop: '2rem'}}>
-							<ItemModal />
-							<GoalList />
-						</Container>
+					<Container style={{ marginTop: '2rem'}}>
+						<Row>
+							<Col>
+								<Calendar />
+							</Col>
+							<Col>
+								<ItemModal />
+								<GoalList />
+							</Col>
+						</Row>
+					</Container>
 					<AppFooter />
 				</div>
 			</Provider>
