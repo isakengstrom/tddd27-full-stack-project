6eb9b3a5f6506c440c1c1264550e5302ed25b517
Engstrom <isaen688@student.liu.se>
2020-12-07 23:20:50 +0100

    Add style to calendar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -8,68 +8,68 @@ import SignInModal from './auth/SignInModal';
 import SignOut from './auth/SignOut';
 
 class AppNavbar extends Component {
-    state = {
-        isOpen: false,
-    }
+  state = {
+    isOpen: false,
+  }
 
-    static propTypes = {
-        auth: PropTypes.object.isRequired,
-    }
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+  }
 
-    toggle = () => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
 
 
-    render() {
-        const { isAuthenticated, user } = this.props.auth;
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
 
-        const authLinks = (
-            <Fragment>
-                <Nav.Item>
-                    <span className="navbar-text mr-3">
-                        <strong>{ user ?  `${user.name}` : '' }</strong>
-                    </span>
-                </Nav.Item>
-                <Nav.Item>
-                    <SignOut />
-                </Nav.Item>
-            </Fragment>
-        );
+    const authLinks = (
+      <Fragment>
+        <Nav.Item>
+          <span className="navbar-text mr-3">
+            <strong>{ user ?  `${user.name}` : '' }</strong>
+          </span>
+        </Nav.Item>
+        <Nav.Item>
+          <SignOut />
+        </Nav.Item>
+      </Fragment>
+    );
 
-        const guestLinks = (
-            <Fragment>
-                <Nav.Item>
-                    <SignUpModal />
-                </Nav.Item>
-                <Nav.Item>
-                    <SignInModal />
-                </Nav.Item>
-            </Fragment>
-        )
+    const guestLinks = (
+      <Fragment>
+        <Nav.Item>
+          <SignUpModal />
+        </Nav.Item>
+        <Nav.Item>
+            <SignInModal />
+        </Nav.Item>
+      </Fragment>
+    )
 
-        return(
-            <div>
-                <Navbar bg="dark" variant="dark" expand="sm">
-                    <Container>
-                        <Navbar.Brand href="/">Coalendar</Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="ml-auto">
-                                {isAuthenticated ? authLinks : guestLinks }
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            </div>
-        ); 
-    }
+    return(
+      <div>
+        <Navbar bg="dark" variant="dark" expand="sm">
+          <Container>
+            <Navbar.Brand href="/">Coalendar</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                  {isAuthenticated ? authLinks : guestLinks }
+                </Nav>
+              </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    ); 
+  }
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, null)(AppNavbar);
